59953e17151a6935ce33f1e909ed494386156f3f
JenniferEinstein <jennifereinstein@gmail.com>
2023-08-27 09:49:35 -0400

    Fix entryController.js. Change entry to entries. Also changed in frontend.

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -14,48 +14,9 @@ const {
     checkDescription,
     checkPatient,
 } = require("../validations/checkEntries");
-
 const checkEntries = require("../validations/checkEntries");
 
 
-const getDistinctPatients = async () => {
-try {
-    const patients = await db.any("SELECT DISTINCT patient FROM entries");
-    return patients.map((entry) => entry.patient);
-} catch (error) {
-    return error;
-}
-};
-
-const searchEntries = async (req, res) => {
-    const {queryType} = req.query;
-    try {
-        if (queryType === "billsByPatient") {
-            const patients = await getDistinctPatients();
-            res.status(200).json(patients)
-        } else if (queryType === "billsToSendToInsurance"){
-            const billsToSend = allEntries.filter((entry) => entry.status !== "done");
-            res.status(200).json(billsToSend);
-        } else if (queryType === "waitingToHearFromInsurance") {
-            const waitingToHear = allEntries.filter((entry) => entry.status == "Sent to insurance");
-            res.status(200).json(waitingToHear);
-        } else if (queryType === "billsFrom2023") {
-            // Implement the logic for this query
-            // You can similarly add more conditions for different query types 
-        } else {
-            // Handle the case where the queryType is not recognized
-            res.status(400).json({ error: "Invalid queryType" });
-          }
-        } catch (error) {
-          res.status(500).json({ error: "Server error" });
-        }
-      };
-
-
-
-
-
-      
 
 // =====  INDEX OF ALL ENTRIES =====
 entries.get("/", async(req,res)=>{
@@ -68,7 +29,7 @@ entries.get("/", async(req,res)=>{
 });
 
 // =====  SHOW ONE ENTRY =====
-entries.get("/entry/:id", async(req,res)=>{
+entries.get("/:id", async(req,res)=>{
     const id = req.params.id;
     const entry = await getEntry(id);
     if(entry) {
@@ -109,24 +70,4 @@ entries.delete('/entry/:id', async(req, res) => {
     }
 });
 
-
-
-// ========== OTHER ===========
-const billsToSendToInsurance = async (req, res) => {
-    try {
-      const allEntries = await getAllEntries();
-      const billsToSend = allEntries.filter(
-        (entry) => entry.status !== "done"
-      );
-  
-      res.status(200).json(billsToSend);
-    } catch (error) {
-      res.status(500).json({ error: "Server error" });
-    }
-  };
-
-module.exports = {
-    entries,
-    billsToSendToInsurance,
-    searchEntries,
-};
+module.exports = entries;
